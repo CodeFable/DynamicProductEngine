93504ae0754568ce387fc6675fa2a0a2e8f019ac
hashirmalikhh@gmail.com <hashirmalikhh@gmail.com>
2026-02-20 23:56:51 +0500

    Map

--- script.js
+++ script.js
@@ -34,7 +34,6 @@ function CardGenerator() {
     const prCategory = document.getElementById('inputCategory').value;
     const prEmoji = document.getElementById('inputEmoji').value;
 
-
     productCards.push({
         name: prName,
         price: prPrice,
@@ -44,37 +43,27 @@ function CardGenerator() {
         emoji: prEmoji
     });
 
-    const HtmlString = data.map(item => `
-    <div class="card" data-category="${productCards.category}">
-        <div class="card-img tone-a">
-            <span>${productCards.emoji}</span>
-        </div>
-
-        <span class="card-tag">${productCards.category}</span>
-
-        <div class="card-body">
-            <div class="card-name">${productCards.name}</div>
-            <div class="card-desc">${productCards.description}</div>
-
-            <div class="card-foot">
-                <span class="card-price">$${productCards.price}</span>
-
-                <span class="card-rating">
-                    <svg viewBox="0 0 24 24">
-                        <polygon
-                          points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2" />
-                    </svg>
-                    ${productCards.rating}
-                </span>
+    // Map and generate cards
+    cardGrid.innerHTML = productCards.map(product => `
+        <div class="card" data-category="${product.category}">
+            <div class="card-img tone-a">
+                <span>${product.emoji}</span>
+            </div>
+            <span class="card-tag">${product.category}</span>
+            <div class="card-body">
+                <div class="card-name">${product.name}</div>
+                <div class="card-desc">${product.description}</div>
+                <div class="card-foot">
+                    <span class="card-price">$${product.price}</span>
+                    <span class="card-rating">
+                        <svg viewBox="0 0 24 24">
+                            <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"/>
+                        </svg>
+                        ${product.rating}
+                    </span>
+                </div>
             </div>
         </div>
-    </div>
-`).join("");
-
-    cardGrid.innerHTML = HtmlString;
-
-
-    console.log(productCards);
+    `).join('');
 }
-
 generateCardBtn.addEventListener('click', CardGenerator)
